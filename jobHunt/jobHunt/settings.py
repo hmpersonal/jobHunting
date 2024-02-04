8b89894ecff5fd02e6a0f5603aad93aaba29458a
hmpersonal <hiroto-morishita-personal@hotmail.com>
2024-02-04 13:36:52 +0900

    django準拠のデータ処理（処理可能、調査中）

--- jobHunt/jobHunt/settings.py
+++ jobHunt/jobHunt/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'jobHunt.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysql', # プロジェクトで使用するデータベース名
+        'USER': 'root', # パソコンにインストールしたMySQLのユーザー名
+        'PASSWORD': 'pswd', # 同上。そのパスワード
     }
 }
 
--- jobHunt/myapp/models.py
+++ jobHunt/myapp/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 
-# Create your models here.
-class AiAnalysisLog(models.Model):
-    autoIncId = models.IntegerField()
-    imagePath = models.CharField(max_length=255)
-    success = models.CharField(max_length=255)
-    message = models.CharField(max_length=255)
-    cls = models.IntegerField()
-    confidence = models.DecimalField(max_digits=5, decimal_places=4)
-    requestTimestamp = models.IntegerField()
-    responseTimeStamp = models.IntegerField()
+class ai_analysis_log(models.Model):
+    id = models.AutoField(primary_key=True)  # イント、11桁、オートインクリメント
+    imagePath = models.CharField(max_length=255, default=None, null=True)  # 文字列、255文字、デフォルトNULL
+    success = models.CharField(max_length=255, default=None, null=True)  # 文字列、255文字、デフォルトNULL
+    message = models.CharField(max_length=255, default=None, null=True)  # 文字列、255文字、デフォルトNULL
+    cls = models.IntegerField(default=None, null=True)  # イント、11桁、デフォルトNULL
+    confidence = models.DecimalField(max_digits=5, decimal_places=4, default=None, null=True)  # デシマル、整数1桁、少数4桁、デフォルトNULL
+    requestTimestamp = models.IntegerField(default=None, null=True)  # イント、10桁、デフォルトNULL
+    responseTimestamp = models.IntegerField(default=None, null=True)  # イント、10桁、デフォルトNULL
 
+    def __str__(self):
+        return f'{self.id} - {self.imagePath}'
